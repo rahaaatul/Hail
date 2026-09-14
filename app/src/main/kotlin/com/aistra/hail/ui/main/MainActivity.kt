3c49cc12fcfe1fc0eab4142581fa9bf3c098a6cf
Rahatul Ghazi <rahatulghazi@gmail.com>
2026-09-14 22:22:06 +0000

    fix: remove unused MaterialTheme and Composable imports

--- app/src/main/kotlin/com/aistra/hail/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/aistra/hail/ui/main/MainActivity.kt
@@ -11,12 +11,10 @@ import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfoV2
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ViewCompositionStrategy
