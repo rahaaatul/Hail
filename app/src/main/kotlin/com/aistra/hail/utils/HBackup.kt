acfdfb1a8d07ccd0adb77b3d33fcb6b7a335b37f
Rahatul Ghazi <rahatulghazi@gmail.com>
2026-09-14 18:02:27 +0000

    fix: resolve Charsets ref and readWhitelistJson data loss (CI #46)

--- app/src/main/kotlin/com/aistra/hail/utils/HBackup.kt
+++ app/src/main/kotlin/com/aistra/hail/utils/HBackup.kt
@@ -13,6 +13,7 @@ import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.FileInputStream
+import java.nio.charset.Charset
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 import java.util.zip.ZipInputStream
@@ -154,7 +155,7 @@ object HBackup {
         val buffer = ByteArray(8192)
         var bytesRead: Int
         while (bufferedStream.read(buffer).also { bytesRead = it } != -1) {
-            stringBuilder.append(String(buffer, 0, bytesRead, "UTF-8".toCharset()))
+            stringBuilder.append(String(buffer, 0, bytesRead, Charset.forName("UTF-8")))
         }
         return stringBuilder.toString()
     }
@@ -176,6 +177,9 @@ object HBackup {
         val jsonArray = JSONArray(readJsonString(bufferedStream))
         for (i in 0 until jsonArray.length()) {
             val pkg = jsonArray.getString(i)
+            if (!HailData.isChecked(pkg)) {
+                HailData.addCheckedApp(pkg, 0, false)
+            }
             HailData.checkedList.firstOrNull { it.packageName == pkg }?.whitelisted = true
         }
         HailData.saveApps()
