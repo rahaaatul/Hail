b06d620c47358923321ff143b6b8776aa259a3ea
Rahatul Ghazi <rahatulghazi@gmail.com>
2026-09-22 09:25:56 +0000

    fix: sort CircleShape import alphabetically in AppGridItem.kt

--- app/src/main/kotlin/com/aistra/hail/ui/home/pager/AppGridItem.kt
+++ app/src/main/kotlin/com/aistra/hail/ui/home/pager/AppGridItem.kt
@@ -10,12 +10,12 @@ import androidx.compose.foundation.layout.absoluteSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectIsPressedAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableInteractionSourceOf
 import androidx.compose.runtime.remember
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
